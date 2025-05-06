4c13bda2f7e5f20dd0035b512f8133285c7f6568
ferribyh <ainsleyhf@gmail.com>
2025-05-06 14:23:34 -0400

    Updated with initial scatterpie map and pull for all media types

--- code/data_pull.R
+++ code/data_pull.R
@@ -4,12 +4,16 @@
 
 #Written by: Hannah Ferriby, hannah.ferriby@tetratech.com
 #Date created: 2025-5-2
-#Date updated: 2025-5-5
+#Date updated: 2025-5-6
 
 
 ####Set Up####
 library(EPATADA)
 library(tidyverse)
+library(sf)
+library(ggplot2)
+library(scales)
+library(scatterpie)
 
 ####Download####
 data <- TADA_DataRetrieval(characteristicName = c('PFOA ion'
@@ -21,15 +25,84 @@ data <- TADA_DataRetrieval(characteristicName = c('PFOA ion'
                                                   , 'Perfluorooctanesulfonate (PFOS)'
                                                   # , 'POTASSIUM PERFLUOROOCTANESULFONATE'
                                                   ),
-                           sampleMedia = c('Water', 'Tissue'), 
+                           # sampleMedia = c('Water', 'Tissue'), 
                            applyautoclean = T)
 
 
-####Filter####
-unique(data$ActivityMediaSubdivisionName)
-
-data_filt <- data %>%
-  filter(ActivityMediaSubdivisionName == 'Surface Water')
-
 ####Export####
-write_csv(data_filt, 'output/data_pull_SW_TISS_ONLY.csv')
+write_csv(data, 'output/data_pull.csv')
+
+####MAPS####
+state_num <- read.table('data/state_codes.txt', header = T, sep = "|", dec = ".") %>%
+  mutate(STATE = ifelse(STATE < 10, as.character(paste0('0',STATE)),
+                        as.character(STATE)))
+
+states <- st_read('data/cb_2018_us_state_500k/cb_2018_us_state_500k.shp') %>%
+  filter(!STATEFP %in% c('60', '66', '69', '78',
+                         '15', '02'))
+
+all_data_all_media <- data %>%
+  left_join(state_num, by = c('StateCode' = 'STATE'))
+
+states_w_data <- all_data_all_media %>%
+  group_by(STATE_NAME) %>%
+  mutate(n_samples_total = n()) %>%
+  ungroup() %>%
+  group_by(STATE_NAME, ActivityMediaName) %>%
+  reframe(STATE_NAME = STATE_NAME, 
+          ActivityMediaName = ActivityMediaName,
+          n_samples_media_type = n(),
+          n_samples_total = n_samples_total) %>%
+  unique() %>%
+  left_join(states, by = c('STATE_NAME'= 'NAME')) %>%
+  mutate(centroid = st_centroid(geometry)) %>%
+  select(STATE_NAME, ActivityMediaName, n_samples_total, n_samples_media_type,
+         centroid, geometry) 
+
+
+#####Scatterpie#####
+filt_pie <- states_w_data %>%
+  select(STATE_NAME, ActivityMediaName, n_samples_media_type,
+         n_samples_total, centroid, geometry) %>%
+  pivot_wider(id_cols = c('STATE_NAME', 'n_samples_total', 'centroid', 'geometry'),
+              names_from = 'ActivityMediaName',
+              values_from = 'n_samples_media_type') %>%
+  mutate(Tissue = ifelse(is.na(Tissue),0,Tissue),
+         Sediment = ifelse(is.na(Sediment),0,Sediment),
+         Soil = ifelse(is.na(Soil),0,Soil),
+         Water = ifelse(is.na(Water),0,Water),
+         Air = ifelse(is.na(Air),0,Air),
+         Other = ifelse(is.na(Other),0,Other)) %>%
+  st_drop_geometry()
+
+filt_pie2 <- extract(filt_pie, centroid, into = c('Lat', 'Lon'), '\\((.*),(.*)\\)', conv = T) %>%
+  filter(!is.na(Lat))
+
+
+ggplot() +
+  geom_sf(data = states, color = 'gray40', fill = 'gray90') +
+  geom_scatterpie(data = as.data.frame(filt_pie2), 
+                  aes(x = Lat, y = Lon, group = STATE_NAME, r = sqrt(n_samples_total)/25), 
+                  cols = c("Tissue", "Water", "Sediment", "Air", "Soil", "Other"),
+                  color = 'black', size = 0.1) +
+  theme_bw() +
+  scale_fill_manual(name = 'Media Type',
+                    values = c(
+                      "Tissue" = "#FF9999",   # Light red
+                      "Water" = "#99CCFF",    # Light blue
+                      "Sediment" = "#FFCC99", # Light orange
+                      "Air" = "#CCCC99",      # Light olive
+                      "Soil" = "#99CC99",     # Light green
+                      "Other" = "#CC99CC"     # Light purple
+                    )) +
+  xlab('')+
+  ylab('')+
+  theme(legend.position = 'top',
+        legend.text = element_text(size = 6),    # Reduces legend text size
+        legend.title = element_text(size = 6),   # Reduces legend title text size
+        legend.key.size = unit(0.5, "lines"),
+        axis.text = element_text(size = 5)) +
+  guides(fill = guide_legend(override.aes = list(size = 0.5))) 
+
+
+ggsave('output/figures/scatterpie_map_all_media.jpg', units = 'in', width = 4.5, height = 4, dpi = 500)
